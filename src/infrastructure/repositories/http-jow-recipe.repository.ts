55137c0a91e899b5a9c8fe18e90ffa99d08b8504
AxelMth <aujoulator@hotmail.fr>
2024-12-27 15:35:59 +0100

    do not valid response

--- src/infrastructure/repositories/http-jow-recipe.repository.ts
+++ src/infrastructure/repositories/http-jow-recipe.repository.ts
@@ -1,18 +1,16 @@
 import { Recipe } from '../../domain/entities/recipe';
-import { jowRecipeSchema } from '../../presentation/schemas/jow-recipe.schema';
 import { axiosInstance } from '../http/axios-instance';
 import { RecipeTargetRepository } from '../../application/ports/output/recipe-target.repository';
 import { JowRecipeAdapter } from '../adapters/jow-recipe.adapter';
 
 export class HttpJowRecipeRepository implements RecipeTargetRepository {
   async saveRecipe(recipe: Recipe): Promise<Recipe> {
-    const response = await axiosInstance.post(`${process.env.JOW_URL}`, JowRecipeAdapter.toJow(recipe), {
+    await axiosInstance.post(`${process.env.JOW_URL}`, JowRecipeAdapter.toJow(recipe), {
       headers: {
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${process.env.JOW_BEARER_TOKEN}`
       }
     });
-    jowRecipeSchema.parse(response.data);
     return recipe
   }
 }
