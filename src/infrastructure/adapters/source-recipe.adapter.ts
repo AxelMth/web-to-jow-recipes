413619b039985d738c5894101bcbb70a4f34aca1
AxelMth <aujoulator@hotmail.fr>
2024-12-30 15:42:38 +0100

    removing pepper

--- src/infrastructure/adapters/source-recipe.adapter.ts
+++ src/infrastructure/adapters/source-recipe.adapter.ts
@@ -7,24 +7,30 @@ import { Duration } from '../../domain/value-objects/duration';
 import { sourceRecipeSchema } from '../../presentation/schemas/source-recipe.schema';
 
 export class SourceRecipeAdapter {
+  private static ingredientsToIgnore = [
+    // Pepper & Salt
+    '63e5122b81b4b295e66f025e',
+  ];
   static toDomain(sourceData: z.infer<typeof sourceRecipeSchema>): Recipe {
     const yields = sourceData.yields.find(y => y.yields === 1);
-    const ingredients = sourceData.ingredients.map(ing => {
-      const foundIngredient = yields?.ingredients.find(
-        yieldIng => yieldIng.id === ing.id
-      );
-      return new Ingredient(
-        ing.id,
-        ing.name,
-        ing.imageLink,
-        {
-          id: foundIngredient?.unit || '',
-          name: foundIngredient?.unit || '',
-          divisor: 1,
-        },
-        foundIngredient?.amount || 0
-      );
-    });
+    const ingredients = sourceData.ingredients
+      .filter(ing => !SourceRecipeAdapter.ingredientsToIgnore.includes(ing.id))
+      .map(ing => {
+        const foundIngredient = yields?.ingredients.find(
+          yieldIng => yieldIng.id === ing.id
+        );
+        return new Ingredient(
+          ing.id,
+          ing.name,
+          ing.imageLink,
+          {
+            id: foundIngredient?.unit || '',
+            name: foundIngredient?.unit || '',
+            divisor: 1,
+          },
+          foundIngredient?.amount || 0
+        );
+      });
     return new Recipe(
       sourceData.id,
       sourceData.name,
